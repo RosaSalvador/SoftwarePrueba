f4a181cde1dc9bc9681fcab0a2bef43613665b24
RosaSalvador <rosa.salvador16@tectijuana.edu.mx>
2021-01-11 21:11:40 -0800

    PruebaCommit

--- P1HolaMundo/app/src/main/java/com/example/p1holamundo/MainActivity.java
+++ P1HolaMundo/app/src/main/java/com/example/p1holamundo/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     //Creación de variables miembro o variables de instancia(ivar)
 
-    private Button mHelloButton;
-    private Button mWorldButton;
+    private Button HelloButton;
+    private Button WorldButton;
 
 
     @Override
@@ -20,21 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //inflar widgets
-        mHelloButton = findViewById(R.id.hello_button);
-        mWorldButton = findViewById(R.id.world_button);
+        HelloButton = findViewById(R.id.hello_button);
+        WorldButton = findViewById(R.id.world_button);
 
         //Programación de evento onClick
-        mHelloButton.setOnClickListener(new View.OnClickListener() {
+        HelloButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,R.string.hello,Toast.LENGTH_SHORT).show();
             }
         });
 
-        mWorldButton.setOnClickListener(new View.OnClickListener() {
+        WorldButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.world,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"HOLAMUNDO PRUWBA 12",Toast.LENGTH_SHORT).show();
             }
         });
     }
